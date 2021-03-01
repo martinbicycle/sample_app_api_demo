7e099369fe7dc8cfa07b4ca65dd1cf265082f3b0
martinbicycle <martinbicycle7700@gmail.com>
2021-03-01 05:36:07 +0000

    [Finish]Google Natural Language API (自然言語処理)

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,8 +4,9 @@ class TodolistsController < ApplicationController
   end
   def create
     list = List.new(list_params)
+    list.score = Language.get_data(list_params[:body])
     list.save
-    tags = Vision.get_image_data(list.image)    
+    tags = Vision.get_image_data(list.image)
     tags.each do |tag|
       list.tags.create(name: tag)
     end
@@ -22,6 +23,7 @@ class TodolistsController < ApplicationController
   end
   def update
     list = List.find(params[:id])
+    list.score = Language.get_data(list_params[:body])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
